6623b493e3df535449ff2cccd02b6aceb3b76552
Ali Nazari <ali.nazari@aut.ac.ir>
2018-05-22 01:04:03 +0430

    newDownload Frame completed but doesn't have add to queue

--- src/NewDownloadFrame.java
+++ src/NewDownloadFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
@@ -7,7 +8,9 @@ import java.io.File;
 
 public class NewDownloadFrame extends JFrame
 {
-    private JPanel 
+    private JPanel frame;
+
+    private JPanel centerPanel;
     private JLabel URLLabel;
     private JTextField URLField;
 
@@ -17,17 +20,21 @@ public class NewDownloadFrame extends JFrame
     private JButton fileChooserButton;
     private JFileChooser fileChooser;
 
+    private JPanel downPanel;
     private JButton confirmButton;
     private JButton cancelButton;
 
     public NewDownloadFrame()
     {
         super("New Download");
-        setLayout(new BorderLayout(10, 10));
         setSize(500, 250);
         setLocationRelativeTo(null);
-        getContentPane().setBackground(new Color(255, 255, 255));
         setDefaultCloseOperation(NewDownloadFrame.HIDE_ON_CLOSE);
+
+        frame = new JPanel(new BorderLayout(10, 10));
+        frame.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        frame.setBackground(Color.decode("#dff8d0"));
+        getContentPane().add(frame);
 
         setComponents();
         showFrame();
@@ -35,22 +42,44 @@ public class NewDownloadFrame extends JFrame
 
     public void setComponents()
     {
-        URLField = new JTextField();
-        URLField.setForeground(new Color(89, 89, 89));
+        centerPanel = new JPanel(new GridLayout(3, 2, 10, 10));
+        centerPanel.setOpaque(false);
 
+        URLLabel = new JLabel("URL: ");
+        centerPanel.add(URLLabel);
+        URLField = new JTextField();
+        centerPanel.add(URLField);
+
+        pathLabel = new JLabel("Save to: ");
+        centerPanel.add(pathLabel);
         pathField = new JTextField("C:\\Users\\Ali_Z\\Desktop");
         pathField.setForeground(new Color(89, 89, 89));
+        centerPanel.add(pathField);
 
         Icon fileChooserIcon = new ImageIcon(getClass().getResource("/Icons/fileChooserIcon.png"));
-        fileChooserButton = new JButton(fileChooserIcon);
-        fileChooserButton.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
-        fileChooserButton.setContentAreaFilled(false);
-        fileChooserButton.setOpaque(false);
-        fileChooserButton.setFocusable(false);
+        fileChooserButton = new JButton("Browse", fileChooserIcon);
+        fileChooserButton.setForeground(Color.decode("#32363f"));
+        fileChooserButton.setFont(new Font("Titillium Web", 4, 20));
         fileChooserButton.addMouseListener(new NewDownloadMouseHandler());
+        centerPanel.add(fileChooserButton);
 
+        frame.add(centerPanel, BorderLayout.CENTER);
 
+        downPanel = new JPanel(new GridLayout(1, 2, 10, 10));
+        downPanel.setOpaque(false);
 
+        cancelButton = new JButton("Cancel");
+        cancelButton.addMouseListener(new NewDownloadMouseHandler());
+        cancelButton.setForeground(Color.decode("#32363f"));
+        cancelButton.setFont(new Font("Titillium Web", 4, 20));
+        downPanel.add(cancelButton);
+        confirmButton = new JButton("Confirm");
+        confirmButton.addMouseListener(new NewDownloadMouseHandler());
+        confirmButton.setForeground(Color.decode("#32363f"));
+        confirmButton.setFont(new Font("Titillium Web", 4, 20));
+        downPanel.add(confirmButton);
+
+        frame.add(downPanel, BorderLayout.SOUTH);
     }
 
     public void setFileChooserHttp()
@@ -61,21 +90,32 @@ public class NewDownloadFrame extends JFrame
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
             System.out.println(selectedFile.getName());
+            pathField.setText(selectedFile.getAbsolutePath());
         }
     }
 
     /**
      * I handle the actions that happen with Mouse here
     */
-    public class NewDownloadMouseHandler extends MouseAdapter
+    private class NewDownloadMouseHandler extends MouseAdapter
     {
         @Override
         public void mouseClicked(MouseEvent e)
         {
             if (e.getSource().equals(fileChooserButton))
             {
-                System.out.println("fileChooserButtonHttp");
+                System.out.println("fileChooserButton");
                 setFileChooserHttp();
+            }
+            else if (e.getSource().equals(confirmButton))
+            {
+                System.out.println("confirmButton");
+            }
+            else if (e.getSource().equals(cancelButton))
+            {
+                System.out.println("cancelButton");
+                setVisible(false); //you can't see me!
+                dispose(); //Destroy the JFrame object
             }
             else
             {
